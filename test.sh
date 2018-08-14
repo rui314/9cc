916237396164ae25557b62bf0d1ca9d9cf8c2070
Rui Ueyama <rui314@gmail.com>
2018-08-14 02:53:18 +0000

    Add "+" and "-".

--- test.sh
+++ test.sh
@@ -8,7 +8,9 @@ try() {
   ./tmp
   actual="$?"
 
-  if [ "$actual" != "$expected" ]; then
+  if [ "$actual" == "$expected" ]; then
+    echo "$input => $actual"
+  else
     echo "$input expected, but got $actual"
     exit 1
   fi
@@ -16,5 +18,6 @@ try() {
 
 try 0 0
 try 42 42
+try 21 '5+20-4'
 
 echo OK
