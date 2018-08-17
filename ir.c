f8a1d0c6903dfe9ab816e719c70efdb27d6f0a4d
Rui Ueyama <rui314@gmail.com>
2018-08-17 01:27:43 +0000

    Add compound statement and return statement.

--- ir.c
+++ ir.c
@@ -1,35 +1,61 @@
 #include "9cc.h"
 
-static IR *new_ir(int op, int lhs, int rhs) {
+static Vector *code;
+
+static IR *add(int op, int lhs, int rhs) {
   IR *ir = malloc(sizeof(IR));
   ir->op = op;
   ir->lhs = lhs;
   ir->rhs = rhs;
+  vec_push(code, ir);
   return ir;
 }
 
-static int gen(Vector *v, Node *node) {
+static int gen_expr(Node *node) {
   static int regno;
 
   if (node->ty == ND_NUM) {
     int r = regno++;
-    vec_push(v, new_ir(IR_IMM, r, node->val));
+    add(IR_IMM, r, node->val);
     return r;
   }
 
   assert(strchr("+-*/", node->ty));
 
-  int lhs = gen(v, node->lhs);
-  int rhs = gen(v, node->rhs);
+  int lhs = gen_expr(node->lhs);
+  int rhs = gen_expr(node->rhs);
 
-  vec_push(v, new_ir(node->ty, lhs, rhs));
-  vec_push(v, new_ir(IR_KILL, rhs, 0));
+  add(node->ty, lhs, rhs);
+  add(IR_KILL, rhs, 0);
   return lhs;
 }
 
+static void gen_stmt(Node *node) {
+  if (node->ty == ND_RETURN) {
+    int r = gen_expr(node->expr);
+    add(IR_RETURN, r, 0);
+    add(IR_KILL, r, 0);
+    return;
+  }
+
+  if (node->ty == ND_EXPR_STMT) {
+    int r = gen_expr(node->expr);
+    add(IR_KILL, r, 0);
+    return;
+  }
+
+  if (node->ty == ND_COMP_STMT) {
+    for (int i = 0; i < node->stmts->len; i++)
+      gen_stmt(node->stmts->data[i]);
+    return;
+  }
+
+  error("unknown node: %d", node->ty);
+}
+
 Vector *gen_ir(Node *node) {
-  Vector *v = new_vec();
-  int r = gen(v, node);
-  vec_push(v, new_ir(IR_RETURN, r, 0));
-  return v;
+  assert(node->ty == ND_COMP_STMT);
+  code = new_vec();
+  gen_stmt(node);
+  return code;
 }
--- token.c
+++ token.c
@@ -1,5 +1,7 @@
 #include "9cc.h"
 
+Map *keywords;
+
 // Tokenizer
 static Token *add_token(Vector *v, int ty, char *input) {
   Token *t = malloc(sizeof(Token));
@@ -10,7 +12,7 @@ static Token *add_token(Vector *v, int ty, char *input) {
 }
 
 // Tokenized input is stored to this array.
-Vector *tokenize(char *p) {
+static Vector *scan(char *p) {
   Vector *v = new_vec();
 
   int i = 0;
@@ -22,10 +24,27 @@ Vector *tokenize(char *p) {
     }
 
     // Single-letter token
-    if (strchr("+-*/", *p)) {
+    if (strchr("+-*/;", *p)) {
       add_token(v, *p, p);
       i++;
       p++;
+      continue;
+    }
+
+    // Keyword
+    if (isalpha(*p) || *p == '_') {
+      int len = 1;
+      while (isalpha(p[len]) || isdigit(p[len]) || p[len] == '_')
+	len++;
+
+      char *name = strndup(p, len);
+      int ty = (intptr_t)map_get(keywords, name);
+      if (!ty)
+	error("unknown identifier: %s", name);
+
+      add_token(v, ty, p);
+      i++;
+      p += len;
       continue;
     }
 
@@ -37,10 +56,16 @@ Vector *tokenize(char *p) {
       continue;
     }
 
-    fprintf(stderr, "cannot tokenize: %s", p);
-    exit(1);
+    error("cannot tokenize: %s", p);
   }
 
   add_token(v, TK_EOF, p);
   return v;
+}
+
+Vector *tokenize(char *p) {
+  keywords = new_map();
+  map_put(keywords, "return", (void *)TK_RETURN);
+
+  return scan(p);
 }
