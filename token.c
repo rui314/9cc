e053c5c857bce891f9235a92231d893b7d0326fc
Rui Ueyama <rui314@gmail.com>
2018-08-19 22:28:20 +0000

    Simplify.

--- token.c
+++ token.c
@@ -2,7 +2,7 @@
 
 // Tokenizer
 static Token *add_token(Vector *v, int ty, char *input) {
-  Token *t = malloc(sizeof(Token));
+  Token *t = calloc(1, sizeof(Token));
   t->ty = ty;
   t->input = input;
   vec_push(v, t);
@@ -18,10 +18,8 @@ static struct {
 };
 
 // Tokenized input is stored to this array.
-static Vector *scan(char *p) {
+Vector *tokenize(char *p) {
   Vector *v = new_vec();
-
-  int i = 0;
 
 loop:
   while (*p) {
@@ -34,12 +32,11 @@ loop:
     // Single-letter token
     if (strchr("+-*/;=(),{}<>", *p)) {
       add_token(v, *p, p);
-      i++;
       p++;
       continue;
     }
 
-    // Multi-letter token
+    // Multi-letter symbol or keyword
     for (int i = 0; symbols[i].name; i++) {
       char *name = symbols[i].name;
       int len = strlen(name);
@@ -47,7 +44,6 @@ loop:
         continue;
 
       add_token(v, symbols[i].ty, p);
-      i++;
       p += len;
       goto loop;
     }
@@ -60,7 +56,6 @@ loop:
 
       Token *t = add_token(v, TK_IDENT, p);
       t->name = strndup(p, len);
-      i++;
       p += len;
       continue;
     }
@@ -68,8 +63,8 @@ loop:
     // Number
     if (isdigit(*p)) {
       Token *t = add_token(v, TK_NUM, p);
-      t->val = strtol(p, &p, 10);
-      i++;
+      for (; isdigit(*p); p++)
+	t->val = t->val * 10 + *p - '0';
       continue;
     }
 
@@ -79,5 +74,3 @@ loop:
   add_token(v, TK_EOF, p);
   return v;
 }
-
-Vector *tokenize(char *p) { return scan(p); }
