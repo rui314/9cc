55f4ed93e1ac0f86b273a43d7d3dc92667c7eaa6
Rui Ueyama <rui314@gmail.com>
2018-08-20 12:10:49 +0000

    Add while loop.

--- gen_ir.c
+++ gen_ir.c
@@ -229,7 +229,7 @@ static int gen_expr(Node *node) {
     int r = nreg++;
     return_reg = r;
 
-    gen_stmt(node->stmt);
+    gen_stmt(node->body);
     label(return_label);
 
     return_label = orig_label;
@@ -278,6 +278,9 @@ static int gen_expr(Node *node) {
 }
 
 static void gen_stmt(Node *node) {
+  if (node->op == ND_NULL)
+    return;
+
   if (node->op == ND_VARDEF) {
     if (!node->init)
       return;
@@ -330,7 +333,7 @@ static void gen_stmt(Node *node) {
     add(IR_UNLESS, r, y);
     kill(r);
     gen_stmt(node->body);
-    kill(gen_expr(node->inc));
+    gen_stmt(node->inc);
     add(IR_JMP, x, -1);
     label(y);
     return;
