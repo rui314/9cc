2d9917b91ecd27f6ba8b219f1b3e9a27e59c554d
Rui Ueyama <rui314@gmail.com>
2018-08-20 09:11:48 +0000

    Implement block scope.

--- sema.c
+++ sema.c
@@ -2,10 +2,30 @@
 
 static Type int_ty = {INT, NULL};
 
-static int str_label;
-static Map *vars;
+typedef struct Env {
+  Map *vars;
+  struct Env *next;
+} Env;
+
 static Vector *globals;
+static int str_label;
 static int stacksize;
+
+static Env *new_env(Env *next) {
+  Env *env = calloc(1, sizeof(Env));
+  env->vars = new_map();
+  env->next = next;
+  return env;
+}
+
+static Var *find(Env *env, char *name) {
+  for (; env; env = env->next) {
+    Var *var = map_get(env->vars, name);
+    if (var)
+      return var;
+  }
+  return NULL;
+}
 
 static void swap(Node **p, Node **q) {
   Node *r = *p;
@@ -21,7 +41,7 @@ static Node *addr_of(Node *base, Type *ty) {
   return node;
 }
 
-static Node *walk(Node *node, bool decay) {
+static Node *walk(Env *env, Node *node, bool decay) {
   switch (node->op) {
   case ND_NUM:
     return node;
@@ -38,10 +58,10 @@ static Node *walk(Node *node, bool decay) {
     ret->op = ND_GVAR;
     ret->ty = node->ty;
     ret->name = var->name;
-    return walk(ret, decay);
+    return walk(env, ret, decay);
   }
   case ND_IDENT: {
-    Var *var = map_get(vars, node->name);
+    Var *var = find(env, node->name);
     if (!var)
       error("undefined variable: %s", node->name);
 
@@ -65,28 +85,28 @@ static Node *walk(Node *node, bool decay) {
     var->ty = node->ty;
     var->is_local = true;
     var->offset = stacksize;
-    map_put(vars, node->name, var);
+    map_put(env->vars, node->name, var);
 
     if (node->init)
-      node->init = walk(node->init, true);
+      node->init = walk(env, node->init, true);
     return node;
   }
   case ND_IF:
-    node->cond = walk(node->cond, true);
-    node->then = walk(node->then, true);
+    node->cond = walk(env, node->cond, true);
+    node->then = walk(env, node->then, true);
     if (node->els)
-      node->els = walk(node->els, true);
+      node->els = walk(env, node->els, true);
     return node;
   case ND_FOR:
-    node->init = walk(node->init, true);
-    node->cond = walk(node->cond, true);
-    node->inc = walk(node->inc, true);
-    node->body = walk(node->body, true);
+    node->init = walk(env, node->init, true);
+    node->cond = walk(env, node->cond, true);
+    node->inc = walk(env, node->inc, true);
+    node->body = walk(env, node->body, true);
     return node;
   case '+':
   case '-':
-    node->lhs = walk(node->lhs, true);
-    node->rhs = walk(node->rhs, true);
+    node->lhs = walk(env, node->lhs, true);
+    node->rhs = walk(env, node->rhs, true);
 
     if (node->rhs->ty->ty == PTR)
       swap(&node->lhs, &node->rhs);
@@ -96,11 +116,11 @@ static Node *walk(Node *node, bool decay) {
     node->ty = node->lhs->ty;
     return node;
   case '=':
-    node->lhs = walk(node->lhs, false);
+    node->lhs = walk(env, node->lhs, false);
     if (node->lhs->op != ND_LVAR && node->lhs->op != ND_DEREF)
       error("not an lvalue: %d (%s)", node->op, node->name);
 
-    node->rhs = walk(node->rhs, true);
+    node->rhs = walk(env, node->rhs, true);
     node->ty = node->lhs->ty;
     return node;
   case '*':
@@ -108,25 +128,25 @@ static Node *walk(Node *node, bool decay) {
   case '<':
   case ND_LOGAND:
   case ND_LOGOR:
-    node->lhs = walk(node->lhs, true);
-    node->rhs = walk(node->rhs, true);
+    node->lhs = walk(env, node->lhs, true);
+    node->rhs = walk(env, node->rhs, true);
     node->ty = node->lhs->ty;
     return node;
   case ND_ADDR:
-    node->expr = walk(node->expr, true);
+    node->expr = walk(env, node->expr, true);
     node->ty = ptr_of(node->expr->ty);
     return node;
   case ND_DEREF:
-    node->expr = walk(node->expr, true);
+    node->expr = walk(env, node->expr, true);
     if (node->expr->ty->ty != PTR)
       error("operand must be a pointer");
     node->ty = node->expr->ty->ptr_of;
     return node;
   case ND_RETURN:
-    node->expr = walk(node->expr, true);
+    node->expr = walk(env, node->expr, true);
     return node;
   case ND_SIZEOF: {
-    Node *expr = walk(node->expr, false);
+    Node *expr = walk(env, node->expr, false);
 
     Node *ret = calloc(1, sizeof(Node));
     ret->op = ND_NUM;
@@ -136,20 +156,22 @@ static Node *walk(Node *node, bool decay) {
   }
   case ND_CALL:
     for (int i = 0; i < node->args->len; i++)
-      node->args->data[i] = walk(node->args->data[i], true);
+      node->args->data[i] = walk(env, node->args->data[i], true);
     node->ty = &int_ty;
     return node;
   case ND_FUNC:
     for (int i = 0; i < node->args->len; i++)
-      node->args->data[i] = walk(node->args->data[i], true);
-    node->body = walk(node->body, true);
+      node->args->data[i] = walk(env, node->args->data[i], true);
+    node->body = walk(env, node->body, true);
     return node;
-  case ND_COMP_STMT:
+  case ND_COMP_STMT: {
+    Env *newenv = new_env(env);
     for (int i = 0; i < node->stmts->len; i++)
-      node->stmts->data[i] = walk(node->stmts->data[i], true);
+      node->stmts->data[i] = walk(newenv, node->stmts->data[i], true);
     return node;
+  }
   case ND_EXPR_STMT:
-    node->expr = walk(node->expr, true);
+    node->expr = walk(env, node->expr, true);
     return node;
   default:
     assert(0 && "unknown node type");
@@ -161,11 +183,10 @@ void sema(Vector *nodes) {
     Node *node = nodes->data[i];
     assert(node->op == ND_FUNC);
 
-    vars = new_map();
     globals = new_vec();
     stacksize = 0;
 
-    walk(node, true);
+    walk(new_env(NULL), node, true);
     node->stacksize = stacksize;
     node->globals = globals;
   }
