f0dd629dcb86123d4de02866897ae53674ccc9a9
Rui Ueyama <rui314@gmail.com>
2018-08-21 14:10:53 +0000

    Make assembly emitter more concise.

--- gen_x86.c
+++ gen_x86.c
@@ -2,7 +2,7 @@
 
 // This pass generates x86-64 assembly from IR.
 
-static int label;
+static int nlabel;
 
 const char *argreg8[] = {"dil", "sil", "dl", "cl", "r8b", "r9b"};
 const char *argreg32[] = {"edi", "esi", "edx", "ecx", "r8d", "r9d"};
@@ -29,24 +29,32 @@ static char *escape(char *s, int len) {
   return sb_get(sb);
 }
 
-void emit_cmp(IR *ir, char *insn) {
-  printf("  cmp %s, %s\n", regs[ir->lhs], regs[ir->rhs]);
-  printf("  %s %s\n", insn, regs8[ir->lhs]);
-  printf("  movzb %s, %s\n", regs[ir->lhs], regs8[ir->lhs]);
+static void emit(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  printf("\t");
+  vprintf(fmt, ap);
+  printf("\n");
+}
+
+static void emit_cmp(char *insn, IR *ir) {
+  emit("cmp %s, %s", regs[ir->lhs], regs[ir->rhs]);
+  emit("%s %s", insn, regs8[ir->lhs]);
+  emit("movzb %s, %s", regs[ir->lhs], regs8[ir->lhs]);
 }
 
 void gen(Function *fn) {
-  char *ret = format(".Lend%d", label++);
+  char *ret = format(".Lend%d", nlabel++);
 
   printf(".global %s\n", fn->name);
   printf("%s:\n", fn->name);
-  printf("  push rbp\n");
-  printf("  mov rbp, rsp\n");
-  printf("  sub rsp, %d\n", roundup(fn->stacksize, 16));
-  printf("  push r12\n");
-  printf("  push r13\n");
-  printf("  push r14\n");
-  printf("  push r15\n");
+  emit("push rbp");
+  emit("mov rbp, rsp");
+  emit("sub rsp, %d", roundup(fn->stacksize, 16));
+  emit("push r12");
+  emit("push r13");
+  emit("push r14");
+  emit("push r15");
 
   for (int i = 0; i < fn->ir->len; i++) {
     IR *ir = fn->ir->data[i];
@@ -55,141 +63,141 @@ void gen(Function *fn) {
 
     switch (ir->op) {
     case IR_IMM:
-      printf("  mov %s, %d\n", regs[lhs], rhs);
+      emit("mov %s, %d", regs[lhs], rhs);
       break;
     case IR_BPREL:
-      printf("  lea %s, [rbp-%d]\n", regs[lhs], rhs);
+      emit("lea %s, [rbp-%d]", regs[lhs], rhs);
       break;
     case IR_MOV:
-      printf("  mov %s, %s\n", regs[lhs], regs[rhs]);
+      emit("mov %s, %s", regs[lhs], regs[rhs]);
       break;
     case IR_RETURN:
-      printf("  mov rax, %s\n", regs[lhs]);
-      printf("  jmp %s\n", ret);
+      emit("mov rax, %s", regs[lhs]);
+      emit("jmp %s", ret);
       break;
     case IR_CALL: {
       for (int i = 0; i < ir->nargs; i++)
-        printf("  mov %s, %s\n", argreg64[i], regs[ir->args[i]]);
+        emit("mov %s, %s", argreg64[i], regs[ir->args[i]]);
 
-      printf("  push r10\n");
-      printf("  push r11\n");
-      printf("  mov rax, 0\n");
-      printf("  call %s\n", ir->name);
-      printf("  pop r11\n");
-      printf("  pop r10\n");
+      emit("push r10");
+      emit("push r11");
+      emit("mov rax, 0");
+      emit("call %s", ir->name);
+      emit("pop r11");
+      emit("pop r10");
 
-      printf("  mov %s, rax\n", regs[lhs]);
+      emit("mov %s, rax", regs[lhs]);
       break;
     }
     case IR_LABEL:
       printf(".L%d:\n", lhs);
       break;
     case IR_LABEL_ADDR:
-      printf("  lea %s, %s\n", regs[lhs], ir->name);
+      emit("lea %s, %s", regs[lhs], ir->name);
       break;
     case IR_NEG:
-      printf("  neg %s\n", regs[lhs]);
+      emit("neg %s", regs[lhs]);
       break;
     case IR_EQ:
-      emit_cmp(ir, "sete");
+      emit_cmp("sete", ir);
       break;
     case IR_NE:
-      emit_cmp(ir, "setne");
+      emit_cmp("setne", ir);
       break;
     case IR_LT:
-      emit_cmp(ir, "setl");
+      emit_cmp("setl", ir);
       break;
     case IR_LE:
-      emit_cmp(ir, "setle");
+      emit_cmp("setle", ir);
       break;
     case IR_AND:
-      printf("  and %s, %s\n", regs[lhs], regs[rhs]);
+      emit("and %s, %s", regs[lhs], regs[rhs]);
       break;
     case IR_OR:
-      printf("  or %s, %s\n", regs[lhs], regs[rhs]);
+      emit("or %s, %s", regs[lhs], regs[rhs]);
       break;
     case IR_XOR:
-      printf("  xor %s, %s\n", regs[lhs], regs[rhs]);
+      emit("xor %s, %s", regs[lhs], regs[rhs]);
       break;
     case IR_SHL:
-      printf("  mov cl, %s\n", regs8[rhs]);
-      printf("  shl %s, cl\n", regs[lhs]);
+      emit("mov cl, %s", regs8[rhs]);
+      emit("shl %s, cl", regs[lhs]);
       break;
     case IR_SHR:
-      printf("  mov cl, %s\n", regs8[rhs]);
-      printf("  shr %s, cl\n", regs[lhs]);
+      emit("mov cl, %s", regs8[rhs]);
+      emit("shr %s, cl", regs[lhs]);
       break;
     case IR_JMP:
-      printf("  jmp .L%d\n", lhs);
+      emit("jmp .L%d", lhs);
       break;
     case IR_IF:
-      printf("  cmp %s, 0\n", regs[lhs]);
-      printf("  jne .L%d\n", rhs);
+      emit("cmp %s, 0", regs[lhs]);
+      emit("jne .L%d", rhs);
       break;
     case IR_UNLESS:
-      printf("  cmp %s, 0\n", regs[lhs]);
-      printf("  je .L%d\n", rhs);
+      emit("cmp %s, 0", regs[lhs]);
+      emit("je .L%d", rhs);
       break;
     case IR_LOAD8:
-      printf("  mov %s, [%s]\n", regs8[lhs], regs[rhs]);
-      printf("  movzb %s, %s\n", regs[lhs], regs8[lhs]);
+      emit("mov %s, [%s]", regs8[lhs], regs[rhs]);
+      emit("movzb %s, %s", regs[lhs], regs8[lhs]);
       break;
     case IR_LOAD32:
-      printf("  mov %s, [%s]\n", regs32[lhs], regs[rhs]);
+      emit("mov %s, [%s]", regs32[lhs], regs[rhs]);
       break;
     case IR_LOAD64:
-      printf("  mov %s, [%s]\n", regs[lhs], regs[rhs]);
+      emit("mov %s, [%s]", regs[lhs], regs[rhs]);
       break;
     case IR_STORE8:
-      printf("  mov [%s], %s\n", regs[lhs], regs8[rhs]);
+      emit("mov [%s], %s", regs[lhs], regs8[rhs]);
     case IR_STORE32:
-      printf("  mov [%s], %s\n", regs[lhs], regs32[rhs]);
+      emit("mov [%s], %s", regs[lhs], regs32[rhs]);
       break;
     case IR_STORE64:
-      printf("  mov [%s], %s\n", regs[lhs], regs[rhs]);
+      emit("mov [%s], %s", regs[lhs], regs[rhs]);
       break;
     case IR_STORE8_ARG:
-      printf("  mov [rbp-%d], %s\n", lhs, argreg8[rhs]);
+      emit("mov [rbp-%d], %s", lhs, argreg8[rhs]);
       break;
     case IR_STORE32_ARG:
-      printf("  mov [rbp-%d], %s\n", lhs, argreg32[rhs]);
+      emit("mov [rbp-%d], %s", lhs, argreg32[rhs]);
       break;
     case IR_STORE64_ARG:
-      printf("  mov [rbp-%d], %s\n", lhs, argreg64[rhs]);
+      emit("mov [rbp-%d], %s", lhs, argreg64[rhs]);
       break;
     case IR_ADD:
-      printf("  add %s, %s\n", regs[lhs], regs[rhs]);
+      emit("add %s, %s", regs[lhs], regs[rhs]);
       break;
     case IR_ADD_IMM:
-      printf("  add %s, %d\n", regs[lhs], rhs);
+      emit("add %s, %d", regs[lhs], rhs);
       break;
     case IR_SUB:
-      printf("  sub %s, %s\n", regs[lhs], regs[rhs]);
+      emit("sub %s, %s", regs[lhs], regs[rhs]);
       break;
     case IR_SUB_IMM:
-      printf("  sub %s, %d\n", regs[lhs], rhs);
+      emit("sub %s, %d", regs[lhs], rhs);
       break;
     case IR_MUL:
-      printf("  mov rax, %s\n", regs[rhs]);
-      printf("  mul %s\n", regs[lhs]);
-      printf("  mov %s, rax\n", regs[lhs]);
+      emit("mov rax, %s", regs[rhs]);
+      emit("mul %s", regs[lhs]);
+      emit("mov %s, rax", regs[lhs]);
       break;
     case IR_MUL_IMM:
-      printf("  mov rax, %d\n", rhs);
-      printf("  mul %s\n", regs[lhs]);
-      printf("  mov %s, rax\n", regs[lhs]);
+      emit("mov rax, %d", rhs);
+      emit("mul %s", regs[lhs]);
+      emit("mov %s, rax", regs[lhs]);
       break;
     case IR_DIV:
-      printf("  mov rax, %s\n", regs[lhs]);
-      printf("  cqo\n");
-      printf("  div %s\n", regs[rhs]);
-      printf("  mov %s, rax\n", regs[lhs]);
+      emit("mov rax, %s", regs[lhs]);
+      emit("cqo");
+      emit("div %s", regs[rhs]);
+      emit("mov %s, rax", regs[lhs]);
       break;
     case IR_MOD:
-      printf("  mov rax, %s\n", regs[lhs]);
-      printf("  cqo\n");
-      printf("  div %s\n", regs[rhs]);
-      printf("  mov %s, rdx\n", regs[lhs]);
+      emit("mov rax, %s", regs[lhs]);
+      emit("cqo");
+      emit("div %s", regs[rhs]);
+      emit("mov %s, rdx", regs[lhs]);
       break;
     case IR_NOP:
       break;
@@ -199,13 +207,13 @@ void gen(Function *fn) {
   }
 
   printf("%s:\n", ret);
-  printf("  pop r15\n");
-  printf("  pop r14\n");
-  printf("  pop r13\n");
-  printf("  pop r12\n");
-  printf("  mov rsp, rbp\n");
-  printf("  pop rbp\n");
-  printf("  ret\n");
+  emit("pop r15");
+  emit("pop r14");
+  emit("pop r13");
+  emit("pop r12");
+  emit("mov rsp, rbp");
+  emit("pop rbp");
+  emit("ret");
 }
 
 void gen_x86(Vector *globals, Vector *fns) {
@@ -217,7 +225,7 @@ void gen_x86(Vector *globals, Vector *fns) {
     if (var->is_extern)
       continue;
     printf("%s:\n", var->name);
-    printf("  .ascii \"%s\"\n", escape(var->data, var->len));
+    emit(".ascii \"%s\"", escape(var->data, var->len));
   }
 
   printf(".text\n");
