29867c9f18f8cd5216d65fb3567da801f6709857
Rui Ueyama <rui314@gmail.com>
2018-08-21 13:21:50 +0000

    Add comment.

--- examples/nqueen.c
+++ examples/nqueen.c
@@ -1,3 +1,9 @@
+// How to run:
+//
+//   $ ./9cc examples/nqueen.c > tmp-nqueen.s
+//   $ gcc -static -o tmp-nqueen tmp-nqueen.s
+//   $ ./tmp-nqueen
+
 int print_board(int *board) {
   for (int i = 0; i < 10; i=i+1) {
     for (int j = 0; j < 10; j=j+1)
